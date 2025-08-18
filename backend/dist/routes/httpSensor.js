ca69e4e9f9f7584bc6b0bb496225090720ab754f
Your Name <user@example.com>
2025-08-19 01:34:01 +0800

    fix

--- backend/dist/routes/httpSensor.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const httpSensorController_1 = require("../controllers/httpSensorController");
-const router = (0, express_1.Router)();
-router.post('/v1/sensor-data', httpSensorController_1.postSensorData);
-exports.default = router;
